cc99392c5155be6a232a304f63b44d196e3a6a44
Mancini <r03fm14@abdn.ac.uk>
2018-02-13 15:50:18 +0000

    First commit

---
+++ Sillyscript.R
@@ -0,0 +1,11 @@
+###############################################
+# Script for Aberdeen Study Group tutorial on 
+# "How to make a Study Group tutorial
+# Author: Francesca Mancini
+# Date created: 2018-02-13
+# Date modified : 
+###############################################
+
+summary(cars)
+
+plot(pressure)
